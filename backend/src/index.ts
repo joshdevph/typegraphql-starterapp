d221ca99cf3ac6c956e2d326c85db9f09343877f
Joshua Alexes Dignadice <joshuadignadice24@gmail.com>
2022-03-08 17:58:23 +0800

    Initial Commit ---> Add Backend and Frontend  Apps

---
+++ backend/src/index.ts
@@ -0,0 +1,46 @@
+import "reflect-metadata"
+import express, {Express} from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from "type-graphql";
+import { UserResolver } from './resolvers/user'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { createConnection } from "typeorm";
+const main =async () => {
+    //Create Connection to Database
+    const connection = await createConnection({
+        type: "postgres",
+        database: "chatapp",
+        entities: [],
+        logging: true,
+        synchronize: true,
+        username: "root", // --- to change as ENV
+        password: "root", // --- to change as ENV
+        port: 5432
+    })
+
+    //Create instance or Apollo Server
+    const apolloServer = new ApolloServer({
+        schema : await buildSchema({
+            resolvers: [UserResolver],
+            validate: false
+        }),
+        //Add Plugin for GraphQL Playground
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground]
+    })
+
+    //Start Apollo Server
+    await apolloServer.start()
+
+    //Initialize Express App
+    const app : Express = express()
+
+    //Create Port to Listen
+    const PORT = 8080
+
+    //Initialize ApolloServer Middleware
+    apolloServer.applyMiddleware({app})
+    app.listen(PORT, () => console.log(`Server is Running on Port:${PORT}`))
+}
+
+//Start Main App
+main().catch((err) => console.log(err))
